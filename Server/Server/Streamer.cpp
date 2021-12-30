f88cb7123e7656b333ad91914ad5f0f649235f66
swk2030 <qlalfqkd23@gmail.com>
2021-12-30 17:38:17 +0900

    feat file transfer

--- Server/Server/Streamer.cpp
+++ Server/Server/Streamer.cpp
@@ -1,8 +1,8 @@
 #include "Streamer.h"
 
-Streamer::Streamer(Network* opaque) {
-    this->cli_sock = opaque->getServerSock();
-    this->serv_addr = opaque->getServerAddr();
+Streamer::Streamer(SOCKET s, struct sockaddr_in s_in) {
+    this->cli_sock = s;
+    this->serv_addr = s_in;
 }
 
 void Streamer::sendData(std::string filename) {
@@ -21,11 +21,15 @@ void Streamer::sendData(std::string filename) {
     fseek(fp, 0, SEEK_END);
     szFile = ftell(fp);
     fseek(fp, 0, SEEK_SET);
+    printf("[LF Server] File size : %d\n", szFile);
     totalSendBytes = 0;
+    _snprintf(buf, sizeof(buf), "%d", szFile);
+    printf("size of buf : %d\n", sizeof(buf));
     sendBytes = send(cli_sock, buf, sizeof(buf), 0);
     while ((sendBytes = fread(buf, sizeof(char), sizeof(buf), fp)) > 0) {
         send(cli_sock, buf, sendBytes, 0);
         totalSendBytes += sendBytes;
+        //printf("In progress: sendbytes %d/%d Byte(s)\n", totalSendBytes, szFile);
     }
     printf("[Streamser] End sending a data to client\n");
     fclose(fp);
--- Server/Server/main.cpp
+++ Server/Server/main.cpp
@@ -2,13 +2,18 @@
 #include "Server.h"
 #include "Client.h"
 #include "Streamer.h"
+#include <time.h>
 int main() {
     Server serv("11000");
     serv.establish_network();
+
+    Streamer stream(serv.getClientSock(), serv.getClientAddr());
+    std::string filenames[] = { "1.mp4", "2.mp4", "3.mp4", "4.mp4", "5.mp4" };
+    for (int i = 0; i < 5; i++) {
+        stream.sendData(filenames[i]);
+        Sleep(2000);
+    }
     serv.cleanNetwork();
 
-    Streamer stream(&serv);
-    stream.sendData("test.mp4");
-
     return 0;
-}
+}       
